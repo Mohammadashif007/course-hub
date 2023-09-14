96bb767e717539adaa024dcc99f824e8a098454f
Mohammad Ashif <mohammadashif510@gmail.com>
2023-09-14 20:57:48 +0600

    courses_container section done

--- src/components/Course_details/Course_details.jsx
+++ src/components/Course_details/Course_details.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React from "react";
+import { FaDollarSign, FaBookOpen } from "react-icons/fa6";
 
-const Course_details = ({course}) => {
-    const {course_name, course_details, image, price, time} = course;
+const Course_details = ({ course }) => {
+    const { course_name, course_details, image, price, time } = course;
     return (
-        <div>
-            <img src={image} alt="" />
-            <h2 className='text-[17px] font-bold my-2'>{course_name}</h2>
-            <p>{course_details}</p>
+        <div className="border p-3 rounded flex-col justify-between">
+            <div>
+                <img src={image} alt="" />
+                <h2 className="text-[17px] font-bold my-2">{course_name}</h2>
+                <p>{course_details}</p>
+            </div>
+            <div className="my-5 flex justify-between items-center">
+                <div className="flex items-center gap-1">
+                    <i>
+                        <FaDollarSign />
+                    </i>
+                    <p>Price : {price}</p>
+                </div>
+                <div className="flex items-center gap-1">
+                    <i>
+                        <FaBookOpen />
+                    </i>
+                    <p>Credit : {time}hr</p>
+                </div>
+            </div>
+            <button className="bg-blue-500 w-full py-2 rounded text-white text-[17px]  ">
+                Select
+            </button>
         </div>
     );
 };
